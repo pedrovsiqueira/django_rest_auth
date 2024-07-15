bb961242b6b929739b2e5d166207ff8b934f3b18
Pedro Siqueira <pedro.v.siqueira@gmail.com>
2024-07-14 22:22:36 -0300

    fix: update allowed hosts for deploy and local

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-p)q=c-=-e0(pozv^1jumsxo!y6wvvl(_-9cu$e0a+ef-kev!rf
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://pedrovsiqueira.pythonanywhere.com/']
+ALLOWED_HOSTS = ['pedrovsiqueira.pythonanywhere.com', '127.0.0.1']
 
 
 # Application definition
